7aa3250336f56534ae771b62778ce60e52ba55bd
Georgette Class-Peters <georgycp@udel.edu>
2020-09-07 17:30:59 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 require 'bundler'
+require 'sqlite3'
 Bundler.require
 
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+# require_relative '../lib/student'
+ 
+# DB = {:conn => SQLite3::Database.new("db/students.db")}
